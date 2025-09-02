f329253956158e045b43ae305f5a657090c86a38
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-09-02 17:31:35 +0530

    initialization added

--- anchor/programs/basic/src/instructions/initialize.rs
+++ anchor/programs/basic/src/instructions/initialize.rs
@@ -1,12 +1,15 @@
 use anchor_lang::prelude::*;
 
-use crate::state::Lottery;
+use crate::state::{Lottery, RewardVault, VaultState};
 
-pub fn initialize(ctx:Context<Initialize>,name:String,organizer: Pubkey, ticket_price : u8, max_participant : u8)->Result<()>{
+pub fn initialize(ctx:Context<Initialize>,name:String,organizer: Pubkey, ticket_price : u64, max_participants : u64, bumps : &InitializeBumps)->Result<()>{
     let lottery = &mut ctx.accounts.lottery;
 
-    lottery.set_inner(Lottery {name, organizer, ticket_price, max_participant, is_active:true });
+    lottery.set_inner(Lottery {name, organizer, ticket_price, max_participants, is_active:true });
 
+    let vault_state = &mut ctx.accounts.vault_state;
+    
+    vault_state.set_inner(VaultState { state_bump: bumps.vault_state, vault_bump: bumps.reward_vault });
     Ok(())
 }
 
@@ -15,6 +18,7 @@ pub fn initialize(ctx:Context<Initialize>,name:String,organizer: Pubkey, ticket_
 pub struct Initialize<'info>{
     #[account(mut)]
     pub organizer : Signer<'info>,
+
     #[account(
         init,
         payer = organizer,
@@ -23,6 +27,24 @@ pub struct Initialize<'info>{
         bump
     )]
     pub lottery : Account<'info,Lottery>,
+
+    #[account(
+        init,
+        payer = organizer,
+        space = 8 + VaultState::INIT_SPACE,
+        seeds = [b"vault",lottery.key().as_ref()],
+        bump 
+    )]
+    pub vault_state : Account<'info,VaultState>,
+
+    #[account(
+        init,
+        payer = organizer,
+        space = 8, 
+        seeds = [b"vault",vault_state.key().as_ref(),lottery.key().as_ref()],
+        bump
+    )]
+    pub reward_vault : Account<'info,RewardVault>,
     pub system_program : Program<'info, System>
 }
 
